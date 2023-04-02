eff93dd389fbdf5d8628ea0f57cb664aa328fef5
Ilay Vinogradov <ilay125114@gmail.com>
2023-04-03 00:28:40 +0300

    added working notification

--- app/src/main/java/ru/surok/myfirstapplication/MainActivity.java
+++ app/src/main/java/ru/surok/myfirstapplication/MainActivity.java
@@ -1,16 +1,25 @@
 package ru.surok.myfirstapplication;
 
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import ru.surok.myfirstapplication.databinding.ActivityMainContraintBinding;
 
 public class MainActivity extends AppCompatActivity {
+
     private ActivityResultLauncher<Intent> startFotRes;
+
+    private final int PERMISSION_REQUEST_CODE = 1111;
     private ActivityMainContraintBinding binding;
 
     @Override
@@ -18,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainContraintBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        createNotificationChannel();
+        requestPermissions();
     }
 
     public void onClickCurrentTrack(View view){
@@ -26,4 +37,34 @@ public class MainActivity extends AppCompatActivity {
         intent.putExtra("song_name", "Bloodhail");
         startActivity(intent);
     }
+
+    public void requestPermissions(){
+        ActivityCompat.requestPermissions(this, new String[] {
+                Manifest.permission.POST_NOTIFICATIONS
+        }, PERMISSION_REQUEST_CODE);
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.CHANNEL_NAME);
+            String description = getString(R.string.CHANNEL_DESCRIPTION);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(getString(R.string.CHANNEL_ID), name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        if (requestCode == PERMISSION_REQUEST_CODE && grantResults.length == 1){
+//            if ()
+//        }
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//    }
 }
--- app/src/main/java/ru/surok/myfirstapplication/PlayBtFragment.java
+++ app/src/main/java/ru/surok/myfirstapplication/PlayBtFragment.java
@@ -1,16 +1,27 @@
 package ru.surok.myfirstapplication;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentResultListener;
 
+import ru.surok.myfirstapplication.databinding.FragmentPlayBtBinding;
+
 public class PlayBtFragment extends Fragment {
+
+    private FragmentPlayBtBinding binding;
 
     public PlayBtFragment() {
         super(R.layout.fragment_play_bt);
@@ -21,25 +32,39 @@ public class PlayBtFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+    @Nullable
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        TextView tv = view.findViewById(R.id.play_bt_textview);
-        tv.setText(R.string.hint_tv);
-        ImageButton bt_play = view.findViewById(R.id.bt_play);
-        bt_play.setOnClickListener(new View.OnClickListener() {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        binding = FragmentPlayBtBinding.inflate(inflater, container,
+                false);
+
+        binding.btPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                tv.setText("Button pressed");
+                showPlayingSongNotification();
+                System.out.println("test");
             }
         });
-
-        getParentFragmentManager().setFragmentResultListener("data_for_playbt_text",
-                this, new FragmentResultListener() {
-                    @Override
-                    public void onFragmentResult(@NonNull String requestKey, @NonNull Bundle result) {
-                        tv.setText(result.getString("text"));
-                    }
-                });
+        return binding.getRoot();
     }
+
+    private void showPlayingSongNotification(){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getActivity(),
+                getString(R.string.CHANNEL_ID))
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle(getString(R.string.next_track))
+                .setContentText(binding.playBtTextview.getText())
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManagerCompat notificationManager = NotificationManagerCompat
+                .from(getActivity());
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.POST_NOTIFICATIONS)
+        == PackageManager.PERMISSION_GRANTED) {
+            notificationManager.notify(1, builder.build());
+        }else{
+            System.out.println("not granted");
+        }
+    }
+
+
 }
